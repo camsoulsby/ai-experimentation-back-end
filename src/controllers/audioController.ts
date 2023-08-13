0747eba6c920976d68a3bed9597788fa07461f42
Cam <cameron.soulsby@gmail.com>
2023-08-13 16:26:28 +1200

    create new route and functions to allow upload of audio file and call review output

--- src/controllers/audioController.ts
+++ src/controllers/audioController.ts
@@ -4,6 +4,7 @@ import transcribeWavToText from "../services/audioProcessing/transcribeWavToText
 import fs, { realpathSync } from "fs";
 import basicTextCompletion from "../services/textProcessing/basicTextCompletion";
 import gpt35ChatCompletion from "../services/textProcessing/gpt35ChatCompletion";
+import annotateCallTranscription from "../services/textProcessing/annotateCallTranscription";
 
 require("dotenv").config();
 
@@ -111,6 +112,45 @@ export const uploadAudioToCode = async (req: Request, res: Response) => {
 
       if (text) {
         const output = await gpt35ChatCompletion(text);
+
+        res.json({ text: output });
+      } else {
+        res.status(500).json({ error: "Failed to transcribe audio" });
+      }
+
+      // Remove the temporary files
+      fs.unlinkSync(filePath);
+      fs.unlinkSync(wavFilePath);
+    });
+  } else {
+    res.status(400).send("No file received");
+  }
+};
+export const uploadAudioCallReview = async (req: Request, res: Response) => {
+  console.log("Received request to /api/upload-audio-call-review");
+
+  if (req.file && req.file.path) {
+    const filePath = req.file.path;
+    const model = "whisper-1";
+
+    // Convert the audio file to wav format
+    convertToWav(filePath, async (err, wavFilePath) => {
+      if (err || !wavFilePath) {
+        console.error("Conversion error:", err);
+        return res
+          .status(500)
+          .json({ error: "Failed to convert audio to WAV" });
+      }
+
+      // Transcribe the WAV file to text
+      const text = await transcribeWavToText(
+        wavFilePath,
+        model,
+        process.env.OPENAI_API_KEY
+      );
+
+      if (text) {
+        const output = await annotateCallTranscription(text);
 
         res.json({ text: output });
       } else {
