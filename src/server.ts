363d75eab0e1dbf812329979bf176eb223df9cd5
Cam <cameron.soulsby@gmail.com>
2024-01-16 06:24:59 +1300

    update to correct port for Heroku

--- src/server.ts
+++ src/server.ts
@@ -38,6 +38,8 @@ app.use("/api", textRoutes);
 //   });
 // });
 
-server.listen(3001, () => {
-  console.log("Server listening at http://localhost:3001");
+const PORT = process.env.PORT || 3001;
+
+server.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
 });
