a776d16a6b5791069c4e3b7830005e12aa884e85
Cam <cameron.soulsby@gmail.com>
2024-01-28 15:25:24 +1300

    update jwtMiddleware to correctly verify token

--- src/middlewares/jwtMiddleware.ts
+++ src/middlewares/jwtMiddleware.ts
@@ -6,6 +6,10 @@ const client = jwksClient({
   jwksUri: "https://login.microsoftonline.com/common/discovery/v2.0/keys",
 });
 
+const expectedAudience = process.env.EXPECTED_AUDIENCE;
+
+console.log("Expected audience", expectedAudience);
+
 // Define an interface that extends Request to include user
 interface RequestWithUser extends Request {
   user?: JwtPayload;
@@ -17,12 +21,32 @@ function getKey(
 ) {
   client.getSigningKey(header.kid as string, function (err, key) {
     if (err) {
+      console.error("Error retrieving signing key:", err);
       callback(err, undefined);
       return;
     }
-    const signingKey =
-      (key as jwksClient.CertSigningKey).publicKey ||
-      (key as jwksClient.RsaSigningKey).rsaPublicKey;
+
+    // Explicitly check if key is undefined or null
+    if (key === undefined || key === null) {
+      console.error("Signing key not found for the given kid");
+      callback(new Error("Signing key not found"), undefined);
+      return;
+    }
+
+    let signingKey: string | Buffer;
+    if ("publicKey" in key) {
+      signingKey = key.publicKey;
+    } else if ("rsaPublicKey" in key) {
+      signingKey = key.rsaPublicKey;
+    } else {
+      console.error("Invalid key format");
+      callback(new Error("Invalid key format"), undefined);
+      return;
+    }
+
+    // console.log("Retrieved signing key ID:", key.kid);
+    // console.log("Retrieved signing key:", signingKey);
+
     callback(null, signingKey);
   });
 }
@@ -41,10 +65,30 @@ export const validateJWT = (
     return res.status(401).send("Access token is required");
   }
 
+  // Safely decode the JWT header with a null check
+  const decodedToken = jwt.decode(token, { complete: true });
+  if (!decodedToken || typeof decodedToken === "string") {
+    console.error("Invalid JWT structure");
+    return res.status(401).send("Invalid token");
+  }
+  console.log("Decoded JWT Header:", decodedToken.header);
+
   jwt.verify(token, getKey, { algorithms: ["RS256"] }, (err, decoded) => {
     if (err) {
       console.error("Token verification error:", err);
       return res.status(401).send("Invalid token");
+    }
+
+    // Check if decoded is undefined or not a JwtPayload object
+    if (!decoded || typeof decoded === "string") {
+      console.error("Invalid token payload");
+      return res.status(401).send("Invalid token payload");
+    }
+
+    // Now TypeScript knows decoded is a JwtPayload object
+    if (decoded.aud !== expectedAudience) {
+      console.error("Token's audience does not match the expected audience");
+      return res.status(401).send("Invalid token audience");
     }
 
     // Use type assertion here
